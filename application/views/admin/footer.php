8efa3c5fe8d2fe21c287ebc044951127f6cfdfdf
hqd276 <dunghq87@gmail.com>
2016-04-15 09:00:44 +0700

    V 0.03

--- application/views/admin/footer.php
+++ application/views/admin/footer.php
@@ -1,9 +1,5 @@
     <hr/>
-<<<<<<< HEAD
     <footer><div style="text-align:center;">© By HD</div></footer>
-=======
-    <footer><div style="text-align:center;">HD</div></footer>
->>>>>>> origin/master
 </div>
 
 </body>
